9b92d5b537cd1003b1b7e71e6a4a2a12b087b06e
Jingle <mbshqqb@qq.com>
2017-11-10 17:57:06 +0800

    增加了GraphX中的几个库，CC,LPA,TC和自己实现的两个：SecondaryDegreeRelationship、SubGraph

--- src/main/scala/com/zj/graphx/LPA.scala
+++ src/main/scala/com/zj/graphx/LPA.scala
@@ -11,20 +11,26 @@ object LPA {
   def main(args: Array[String]): Unit = {
     case class VD(){}
     case class ED(){}
-    //    val conf = new SparkConf().setAppName("LPA").setMaster("local[3]")
+        val conf = new SparkConf().setAppName("LPA").setMaster("local[3]")
 //    val conf = new SparkConf().setAppName("LPA").setMaster("spark://192.168.52.165:7077")
-    val conf = new SparkConf().setAppName("LPA").setMaster("spark://127.0.0.1:7077")
+//    val conf = new SparkConf().setAppName("LPA").setMaster("spark://127.0.0.1:7077")
     val sparkContext = SparkContext.getOrCreate(conf)
-    //    val edges: RDD[Edge[ED]] = sparkContext.textFile(LPA.getClass.getResource("/twitter/twitter.csv").getPath).map(line => {
-
-    val edges: RDD[Edge[ED]] = sparkContext.textFile("twitter.csv").map(line => {
-      val tokens = line.split(" ")
-      Edge[ED](tokens(0).toLong, tokens(1).toLong, null)
+        val edges: RDD[Edge[ED]] = sparkContext.textFile(LPA.getClass.getResource("/twitter/twitter.csv").getPath)flatMap(line => {
+//    val edges: RDD[Edge[ED]] = sparkContext.textFile("twitter.csv").flatMap(line => {
+      try{
+        val tokens = line.split(" ")
+        Option(Edge[ED](tokens(0).toLong, tokens(1).toLong, null))
+      }catch {
+        case ex=>{
+          ex.printStackTrace()
+          None
+        }
+      }
     })
     val graph: Graph[VD, ED] = Graph.fromEdges[VD, ED](edges,null)
     val vWriter = new PrintWriter(new File("LAP.csv"))
-    val lpaGraph:Graph[VertexId, ED]=LabelPropagation.run(graph,1000)
-    lpaGraph.vertices.foreach(println)
+    val lpaGraph:Graph[VertexId, ED]=LabelPropagation.run(graph,10)
+    lpaGraph.vertices.collect.foreach(println)
     vWriter.close()
   }
 }
--- src/main/scala/com/zj/graphx/TC.scala
+++ src/main/scala/com/zj/graphx/TC.scala
@@ -10,20 +10,19 @@ object TC {
   def main(args: Array[String]): Unit = {
     case class VD(){}
     case class ED(){}
-//    val conf = new SparkConf().setAppName("TC").setMaster("local[3]")
+    val conf = new SparkConf().setAppName("TC").setMaster("local[3]")
 //    val conf = new SparkConf().setAppName("TC").setMaster("spark://192.168.52.165:7077")
-    val conf = new SparkConf().setAppName("TC").setMaster("spark://127.0.0.1:7077")
+//    val conf = new SparkConf().setAppName("TC").setMaster("spark://127.0.0.1:7077")
     val sparkContext = SparkContext.getOrCreate(conf)
-//    val edges: RDD[Edge[ED]] = sparkContext.textFile(TC.getClass.getResource("/twitter/twitter.csv").getPath).map(line => {
+    val edges: RDD[Edge[ED]] = sparkContext.textFile(TC.getClass.getResource("/twitter/twitter.csv").getPath).map(line => {
 
-    val edges: RDD[Edge[ED]] = sparkContext.textFile("twitter.csv").map(line => {
+//    val edges: RDD[Edge[ED]] = sparkContext.textFile("twitter.csv").map(line => {
     val tokens = line.split(" ")
       Edge[ED](tokens(0).toLong, tokens(1).toLong, null)
     })
     val graph: Graph[VD, ED] = Graph.fromEdges[VD, ED](edges,null)
     val vWriter = new PrintWriter(new File("TC.csv"))
-   val tcGraph:Graph[Int, ED]=graph.triangleCount()
-    tcGraph.vertices.foreach(vWriter.println)
+   graph.triangleCount().vertices.collect.foreach(vWriter.println)
     vWriter.close()
   }
 }
